49f815538065d384ec535661b75f097103b18278
Skyshock <cesara.gonzalez1997@gmail.com>
2021-09-20 23:16:54 -0500

    Login - Fixing - Set Permission

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -7,5 +7,5 @@ export default {
     const password = await bcrypt.hash(pwd, 10);
     return await ctx.prisma.updateUser({ data: { password }, where: { token}});
   },
-  updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args), 
+  updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
 };
