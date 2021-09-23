fbfd13987941eeb7137215b9394a5064b84a0276
Skyshock <cesara.gonzalez1997@gmail.com>
2021-09-22 19:19:24 -0500

    Resolve Expose

--- src/resolvers/Mutation/user.ts
+++ src/resolvers/Mutation/user.ts
@@ -1,11 +1,24 @@
 import * as bcrypt from "bcryptjs";
+import { UserCreateInput } from "../../generated/prisma-client";
 import { Context } from "../../utils";
+import * as randomstring from 'randomstring'
 
 export default {
   async setPwd(parent, args, ctx: Context) {
     const { pwd, token } = args.data;
     const password = await bcrypt.hash(pwd, 10);
-    return await ctx.prisma.updateUser({ data: { password }, where: { token}});
+    return await ctx.prisma.updateUser({ data: { password }, where: { token }});
   },
-  updateUser: (parent, args, ctx: Context) => ctx.prisma.updateUser(args),
+  updateUser: (parent, args: { data: any, where: any}, ctx: Context) => ctx.prisma.updateUser(args),
+  createUser: async (parent, { data }: { data: UserCreateInput}, ctx: Context) => {
+    const token = randomstring.generate(8).toUpperCase()
+    const salt = bcrypt.genSaltSync(10)
+    const password = await bcrypt.hashSync(data.password, salt)
+    data = {
+      token,
+      ...data,
+      password
+    }
+  return ctx.prisma.createUser(data)
+  }
 };
